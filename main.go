956fb788ffd604db03938dfb7f2288701f341aa3
yiziaoxue <775777006@qq.com>
2018-08-25 15:10:25 +0800

    优化

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	_ "gba/common"
     "github.com/gin-gonic/gin"
     "github.com/gin-contrib/cors"
+	"os"
+	"log"
 )
 
 func main() {
@@ -19,5 +21,12 @@ func main() {
 	router.Static("/app", "./app")//加载静态资源
 	inits(router)//初始化
 	router.Run(":8000")
+
+	fileName := "/data1/logs/golang.log"
+	logFile,err  := os.Create(fileName)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error !")
+	}
 }
 
